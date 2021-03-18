d55fbdeaa246834f7d270cef2664bd2f30f264c0
unknown <smartfriendmohit@gmail.com>
2021-03-18 09:47:26 +0530

    completed the project

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,31 +1,34 @@
-import React from "react";
-import PageHeader from "../pageHeader/PageHeader";
-import classes from "./About.module.css";
-import me from "../../images/profile1.jpg";
+import React from 'react'
+import PageHeader from '../pageHeader/PageHeader'
+import classes from './About.module.css'
+import me from '../../images/profile2.jpg'
 
 const About = () => {
   return (
-    <div className={classes.aboutme} id="about">
-      <PageHeader title={"About Me"} />
+    <div className={classes.aboutme} id='about'>
+      <PageHeader title={'About Me'} />
       <div className={classes.container}>
         <div className={classes.text}>
           <h2>Hi! I'm Mohit Tyagi</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
-            facere soluta atque iusto facilis, explicabo sapiente doloremque
-            assumenda iste maiores repellat quaerat corrupti eaque laudantium,
-            delectus illo obcaecati amet nulla. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Recusandae, facere soluta atque iusto
-            facilis, explicabo sapiente doloremque assumenda iste maiores
-            repellat quaerat corrupti eaque laudantium, delectus illo obcaecati
-            amet nulla.
+            I am a full stack developer currently working in iBirds Software
+            Services Pvt. Ltd, Ajmer, India. I have worked in many projects
+            including technologies like Java, Node Js, React JS, LWC, HTML, CSS,
+            Salesforce, PostgreSQL, MySQL, MongoDB, Azure, Hiroku, Firebase and
+            what not
+            <span style={{ fontSize: '100%' }}>&#128517;</span>.
+          </p>
+          <br />
+          <p>
+            I like to learn about new technologies. I LOVE to play online games
+            like Lineage2 and Elder Scrolls Online.
           </p>
         </div>
         <div className={classes.photo}>
-          <img className={classes.me} src={me} alt="me" />
+          <img className={classes.me} src={me} alt='me' />
         </div>
       </div>
     </div>
-  );
-};
-export default About;
+  )
+}
+export default About
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,52 +1,54 @@
-import React from "react";
-import PageHeader from "../pageHeader/PageHeader";
-import classes from "./Skills.module.css";
-import codeThinking from "../../images/undraw_Code_thinking_re_gka2.svg";
+import React from 'react'
+import PageHeader from '../pageHeader/PageHeader'
+import classes from './Skills.module.css'
+import codeThinking from '../../images/undraw_Code_thinking_re_gka2.svg'
 
 const Skills = () => {
   const listTitleStyle = {
     fontWeight: 900,
-    color: "#9B1FE8",
-    marginBottom: "4px",
-  };
+    color: '#9B1FE8',
+    marginBottom: '4px',
+  }
   const frontEndSkills = (
     <ul>
       <li style={listTitleStyle}>FRONTEND</li>
       <li>ReactJs - Redux - HTML5 - CSS3</li>
     </ul>
-  );
+  )
   const backEndSkills = (
     <ul>
       <li style={listTitleStyle}>BACKEND</li>
       <li>Java - NodeJS - SQL - MongoDB</li>
       <li>Spring Boot - Express - REST</li>
     </ul>
-  );
+  )
 
   const otherSkills = (
     <ul>
       <li style={listTitleStyle}>OTHER</li>
       <li>Git - JSON - XML</li>
+      <li>Azure - Heroku - Netlify - Firebase</li>
     </ul>
-  );
+  )
 
-  const totalSkills = [frontEndSkills, backEndSkills, otherSkills];
+  const totalSkills = [frontEndSkills, backEndSkills, otherSkills]
   return (
-    <div className={classes.skills} id="skills">
-      <PageHeader title={"What about the skills?"} />
+    <div className={classes.skills} id='skills'>
+      <PageHeader title={'What about the skills?'} />
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-        neque quos doloremque modi est ipsam magnam dolorem totam. Quaerat
-        similique dolorem dolorum, doloremque ullam quae odio obcaecati error
-        aliquid quod.
+        These are some of the skills I have aquired while working on projects
+        personally and officially. I got the chance to put my hands on many
+        technologies and some serious functionalities like integrations while
+        working with my current employer. These are some skills I feel
+        comfortable in..
       </p>
       <div className={classes.container}>
-        <img src={codeThinking} alt="code thinking" />
+        <img src={codeThinking} alt='code thinking' />
         {totalSkills.map((skill) => {
-          return <div className={classes.list}>{skill}</div>;
+          return <div className={classes.list}>{skill}</div>
         })}
       </div>
     </div>
-  );
-};
-export default Skills;
+  )
+}
+export default Skills
